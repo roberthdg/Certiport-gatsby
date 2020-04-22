82e1f4c677db0a15d56f166fcdd188264be12929
Roberth <roberthdgg@gmail.com>
2020-04-21 20:34:10 -0400

    Added mail subscription API connection

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Grid from '@material-ui/core/Grid';
 import RoomOutlinedIcon from '@material-ui/icons/RoomOutlined';
 import LocalPhoneOutlinedIcon from '@material-ui/icons/LocalPhoneOutlined';
 import MailOutlineOutlinedIcon from '@material-ui/icons/MailOutlineOutlined';
 import TextField from '@material-ui/core/TextField';
 import { makeStyles } from '@material-ui/core/styles';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 const useStyles = makeStyles({
     multilineColor:{
@@ -21,6 +22,46 @@ const useStyles = makeStyles({
 
 export default function Content(props) {
     const classes = useStyles();
+    const [email, setEmail] = useState('');
+    const [displayAlert, setDisplayAlert] = useState(false);
+    const [alertMessage, setAlertMessage] = useState('');
+    const [displayLoader, setDisplayLoader] = useState(false);
+    const [disableField, setDisableField] = useState(false);
+    const regexEmail = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+   
+    function subscribe() {
+        setDisplayAlert(false);
+        
+        if(email==='' || regexEmail.test(email)===false) {
+            setDisplayAlert(true);
+            setAlertMessage(['warning', 'Por favor ingrese  una dirección de correo válida']);
+        }
+        else {
+            setDisplayLoader(true);
+            setDisableField(true);
+            fetch(`https://fletesya.cl/api/mail`, {
+                headers:{
+                    'Content-Type': 'application/json'
+                  },
+                method:'POST',
+                body: JSON.stringify({mail: email, yachana: true})
+            })
+            .then(res => {
+                setEmail('');
+                setDisplayAlert(true);
+                setAlertMessage(['success','Suscripción realizada exitosamente']);
+                setDisplayLoader(false);
+                setDisableField(false);
+            })
+            .catch(error => {
+                setDisplayLoader(false);
+                setDisableField(false);
+                setDisplayAlert(true);
+                setAlertMessage(['error', 'Error de conexión']);
+            })
+        }
+    }
+
     return (            
         <div id='contacto'>
             <img src='/bottom.png' draggable={false} style={{width:'100vw', marginTop: '-2px'}}/>
@@ -48,9 +89,16 @@ export default function Content(props) {
                 <Grid item lg={3} sm={11} style={{margin:'0 auto', maxWidth:'85%'}}>
                     <p style={{fontSize:'22px', fontFamily:'fellix'}}>
                         ¿Quieres recibir notificaciones sobre nuevos cursos y programas de certificación?<br/><strong>Suscríbete</strong><br/><br/>                 
+                        { displayLoader? <div className='loader' style={{marginTop:'0px', marginBottom:'20px'}}> <CircularProgress size='50px' style={{color:'white'}} /> </div> : null}
+                        { displayAlert
+                        ? <div style={{fontSize:'16px', maxWidth:'80%', margin: 'auto'}}> <strong style={alertMessage[0]==='success'? {color:'white'} : {color: 'red'}}> {alertMessage[1]} </strong> <br /> <br /> </div>     
+                        : null }
                         <TextField
                             label="Correo electrónico"
                             variant="filled"
+                            value={email}
+                            disabled={disableField}
+                            onChange={e => setEmail(e.target.value)} 
                             InputLabelProps={{
                                 className: classes.floatingLabelFocusStyle,
                                 }}
@@ -61,7 +109,7 @@ export default function Content(props) {
                                 className: classes.multilineColor
                             }}
                         /> <br /> <br />
-                        <a style={{padding:'15px', margin:'10px', backgroundColor:'white', fontSize:'15px', color:'black', borderRadius:'3px', cursor:'pointer'}}>ENVIAR</a> 
+                        <a onClick={() => displayLoader? null: subscribe()} style={{color:'white'}} style={{padding:'15px', margin:'10px', backgroundColor:'rgb(10,113,144)', fontSize:'15px', color:'white', borderRadius:'3px', cursor:'pointer'}}>ENVIAR</a> 
                     </p>
                 </Grid>
             </Grid>
